1a96dbadf93ed344c2d1b5f95fe592f4541ce3ed
D14na <support@d14na.org>
2019-07-14 17:02:59 -0400

    Added session logging.

--- main.js
+++ main.js
@@ -1,21 +1,134 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-// const io = require('socket.io')(http)
-
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 3000 })
-
+const http = require('http')
 const nano = require('nano')('http://localhost:5984')
+const node_static = require('node-static')
+const sockjs = require('sockjs')
 const winston = require('winston')
 
 const ethers = require('ethers')
 const moment = require('moment')
 
 /* Initialize database connection. */
-const dbConn = nano.db.use('nametag')
+// const dbConn = nano.db.use('ministo')
+// const dbConn = nano.db.use('ministo-ropsten')
+const dbConn = nano.db.use('ministo_sessions')
 
 /* Set keep-alive interval. */
 const KEEP_ALIVE_INTERVAL = 15000
+
+/* Set port number. */
+const PORT = 3000
+
+/* Initialize connections pool. */
+let connPool = {}
+
+/* Initialize SockJS (fallback) URL. */
+const sockjs_url = 'https://cdn.0net.io/libs/sockjs-client/1.1.4/js/sockjs.min.js'
+
+/* Create SockJS (WebSocket) server. */
+const ws = sockjs.createServer({ sockjs_url })
+
+/* Initialize error listener. */
+ws.on('error', (_err) => {
+    console.error('WebSockets fatal error', _err)
+})
+
+/* Handle new client connection. */
+ws.on('connection', async (_conn) => {
+    // console.log('CONN', _conn)
+
+    /* Set connection id. */
+    const connId = _conn.id
+
+    /* Add/update connection in pool. */
+    connPool[connId] = _conn
+
+    /* Build session (for database). */
+    const session = {
+        id: _conn.id,
+        headers: _conn.headers,
+        protocol: _conn.protocol
+    }
+
+    /* Insert into (success) database. */
+    let result = await dbConn.insert(session)
+        .catch(_error => {
+            console.error('DB ERROR:', _error)
+        })
+
+    console.log('DB RESULT', result)
+
+    /* Initialize error listener. */
+    // _conn.on('error', _handleError)
+
+    /* Initialize close listener. */
+    // _conn.on('close', _handleClose)
+
+    /* Initialize data (message) listener. */
+    _conn.on('data', (_data) => {
+        /* Initialize data. */
+        let data = null
+
+        /* Protect server process from FAILED parsing. */
+        try {
+            /* Parse the incoming data. */
+            data = JSON.parse(_data)
+        } catch (_err) {
+            return console.log('Error parsing incoming data', _data)
+        }
+
+        console.log('DATA (parsed)', data)
+
+        let connCount = Object.keys(connPool).length
+
+        let note = {
+            id: connId,
+            welcome: 'please be patient...',
+            connCount: connCount
+        }
+
+        _conn.write(JSON.stringify(note))
+
+        /* Retrieve connection id. */
+        // const connId = _conn.id
+
+        /* Retrieve data id. */
+        // const dataId = data.dataId
+
+        /* Validate connection id. */
+        // if (!connId) {
+        //     return console.error('Could not retrieve connection id for:', _data)
+        // }
+    })
+})
+
+/* Initialize static files. */
+const static_directory = new node_static.Server(__dirname)
+
+const server = http.createServer()
+
+server.addListener('request', function (req, res) {
+    static_directory.serve(req, res)
+})
+
+server.addListener('upgrade', function (req, res) {
+    res.end()
+})
+
+/* Install WebSocket server handlers. */
+ws.installHandlers(server)
+
+/* Start listening (for incoming CLIENT connections). */
+// NOTE Localhost proxy via Nginx (providing TLS/SSL).
+server.listen(PORT, '0.0.0.0')
+
+
+
+// const app = require('express')()
+// const http = require('http').createServer(app)
+// const io = require('socket.io')(http)
+
+// const WebSocket = require('ws')
+// const wss = new WebSocket.Server({ port: 3000 })
 
 // io.on('connection', function (_socket) {
 //     // console.log('an user connected', _socket)
@@ -73,75 +186,75 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // Broadcast to all.
-wss.broadcast = function (_data) {
-    wss.clients.forEach(function (_client) {
-        if (_client.readyState === WebSocket.OPEN) {
-            _client.send(_data)
-        }
-    })
-}
+// wss.broadcast = function (_data) {
+//     wss.clients.forEach(function (_client) {
+//         if (_client.readyState === WebSocket.OPEN) {
+//             _client.send(_data)
+//         }
+//     })
+// }
 
-wss.on('close', () => {
-    console.log('Closing client connection.')
-})
+// wss.on('close', () => {
+//     console.log('Closing client connection.')
+// })
 
-wss.on('connection', function (_ws, _req) {
-    /* Log connection source (ip address). */
-    if (_req.headers && _req.headers['x-forwarded-for']) {
-        const ip = _req.headers['x-forwarded-for'].split(/\s*,\s*/)[0]
-
-        logger.info(`Connection from [ ${ip} ]`)
-    }
-
-    /* Set flag. */
-    _ws.isAlive = true
-
-    /* Handle pong. */
-    _ws.on('pong', _heartbeat)
-
-    /* Handle message. */
-    _ws.on('message', function (_message) {
-        logger.debug(`received: [ ${_message} ]`)
-    })
-
-    /* Set action. */
-    const action = 'init'
-
-    /* Set address. */
-    const address = '0xaE6A6bfDe0B226302ccA6155f487D1f46e6AC821'
-
-    /* Set challenge. */
-    // const challengeNum = ethers.utils.bigNumberify(
-    //     '71152175942061195429752053136004011344333682236262982809903143900646877130177')
-    // const challenge = challengeNum.toHexString()
-    const challenge = '0x8a4ef18d3b802dab9164b66ee1b07dc8b64eec8a7b3722bb09a053d832b97086'
-
-    /* Set difficulty. */
-    const difficulty = 1
-
-    /* Set target. */
-    // const target = '27606985387162255149739023449108101809804435888681546220650096895197184'
-    // const target = '441711766194596082395824375185729628956870974218904739530401550323154944' // 64
-    // const target = '11579208923731619542357098500868790785326998466564056403945758400791312963993' // 256
-    // const target = '0x04000000000000000000000000000000000000000000000000000000000000' // easy solve
-    const target = '0x040000000000000000000000000000000000000000000000000000000000' // 0xToken diff-1
-
-    /* Set metadata. */
-    const metadata = 'ZeroGold - 0GOLD'
-
-    /* Build welcome package. */
-    const pkg = {
-        action,
-        address,
-        challenge,
-        difficulty,
-        target,
-        metadata
-    }
-
-    /* Send welcome package. */
-    _ws.send(JSON.stringify(pkg))
-})
+// wss.on('connection', function (_ws, _req) {
+//     /* Log connection source (ip address). */
+//     if (_req.headers && _req.headers['x-forwarded-for']) {
+//         const ip = _req.headers['x-forwarded-for'].split(/\s*,\s*/)[0]
+//
+//         logger.info(`Connection from [ ${ip} ]`)
+//     }
+//
+//     /* Set flag. */
+//     _ws.isAlive = true
+//
+//     /* Handle pong. */
+//     _ws.on('pong', _heartbeat)
+//
+//     /* Handle message. */
+//     _ws.on('message', function (_message) {
+//         logger.debug(`received: [ ${_message} ]`)
+//     })
+//
+//     /* Set action. */
+//     const action = 'init'
+//
+//     /* Set address. */
+//     const address = '0xaE6A6bfDe0B226302ccA6155f487D1f46e6AC821'
+//
+//     /* Set challenge. */
+//     // const challengeNum = ethers.utils.bigNumberify(
+//     //     '71152175942061195429752053136004011344333682236262982809903143900646877130177')
+//     // const challenge = challengeNum.toHexString()
+//     const challenge = '0x8a4ef18d3b802dab9164b66ee1b07dc8b64eec8a7b3722bb09a053d832b97086'
+//
+//     /* Set difficulty. */
+//     const difficulty = 1
+//
+//     /* Set target. */
+//     // const target = '27606985387162255149739023449108101809804435888681546220650096895197184'
+//     // const target = '441711766194596082395824375185729628956870974218904739530401550323154944' // 64
+//     // const target = '11579208923731619542357098500868790785326998466564056403945758400791312963993' // 256
+//     // const target = '0x04000000000000000000000000000000000000000000000000000000000000' // easy solve
+//     const target = '0x040000000000000000000000000000000000000000000000000000000000' // 0xToken diff-1
+//
+//     /* Set metadata. */
+//     const metadata = 'ZeroGold - 0GOLD'
+//
+//     /* Build welcome package. */
+//     const pkg = {
+//         action,
+//         address,
+//         challenge,
+//         difficulty,
+//         target,
+//         metadata
+//     }
+//
+//     /* Send welcome package. */
+//     _ws.send(JSON.stringify(pkg))
+// })
 
 /* Start broadcasting. */
 // const interval = setInterval(() => {
@@ -151,26 +264,26 @@ wss.on('connection', function (_ws, _req) {
 // }, KEEP_ALIVE_INTERVAL)
 
 /* Start ping interval. */
-const interval = setInterval(function () {
-    /* Ping each client. */
-    wss.clients.forEach(function (_ws) {
-        console.log('searching..')
-
-        /* Test for recent heartbeat. */
-        if (_ws.isAlive === false) {
-            console.log('Terminating connection to', _ws)
-
-            /* Terminate connection. */
-            return _ws.terminate()
-        }
-
-        /* Set flag. */
-        _ws.isAlive = false
-
-        /* Ping client. */
-        _ws.ping(_noop)
-    })
-}, KEEP_ALIVE_INTERVAL)
+// const interval = setInterval(function () {
+//     /* Ping each client. */
+//     wss.clients.forEach(function (_ws) {
+//         console.log('searching..')
+//
+//         /* Test for recent heartbeat. */
+//         if (_ws.isAlive === false) {
+//             console.log('Terminating connection to', _ws)
+//
+//             /* Terminate connection. */
+//             return _ws.terminate()
+//         }
+//
+//         /* Set flag. */
+//         _ws.isAlive = false
+//
+//         /* Ping client. */
+//         _ws.ping(_noop)
+//     })
+// }, KEEP_ALIVE_INTERVAL)
 
 const submitShare = async function (args, callback) {
     /* Initialize valid JSON flag. */
